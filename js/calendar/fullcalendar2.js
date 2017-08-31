a9c4109c18133d0caf8bcda4b52c452ba9a75b1e
Besaru <besaru@gmail.com>
2017-08-31 18:11:47 +0200

    Img->Proyecto,Macrobiotica,Agenda

--- js/calendar/fullcalendar2.js
+++ js/calendar/fullcalendar2.js
@@ -107,55 +107,15 @@
       }
 
     },
-
-    events: [
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d-3, 16, 0),
-        allDay: false,
-        className: 'info'
-      },
-      {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d+4, 16, 0),
-        allDay: false,
-        className: 'info'
-      },
-      {
-        title: 'Meeting',
-        start: new Date(y, m, d, 10, 30),
-        allDay: false,
-        className: 'important'
-      },
-      {
-        title: 'Lunch',
-        start: new Date(y, m, d, 12, 0),
-        end: new Date(y, m, d, 14, 0),
-        allDay: false,
-        className: 'important'
-      },
-      {
-        title: 'Birthday Party',
-        start: new Date(y, m, d+1, 19, 0),
-        end: new Date(y, m, d+1, 22, 30),
-        allDay: false,
-      },
-      {
-        title: 'Click for Google',
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        url: 'http://google.com/',
-        className: 'success'
-      }
-    ],
-  });
-
-  $('#calendar').fullCalendar({
-
     eventSources: [
         '../js/calendar/source.json'
+
     ],
   });
+//  $('#calendar').fullCalendar({
+
+//    eventSources: [
+//        '../js/calendar/source.json'
+//    ],
+//  });-->
     });
